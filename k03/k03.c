311710e754c7eee793274d6e6d8f12ceee6afec6
Kaito Ishizuka <bb35319007@ms.nagasaki-u.ac.jp>
2020-12-09 19:46:56 +0900

    k03提出

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -10,13 +12,64 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+size_t text_len = strlen(text);
+size_t key_len = strlen(key);
 
+if (text_len < key_len)
+{
+    return NULL;
 }
+for (size_t text_i = 0, E = text_len - key_len; text_i <= E; ++text_i)
+{
+    bool match = true;
+     for (size_t key_i = 0; key_i < key_len; ++key_i) {
+            if (text[text_i + key_i] != key[key_i]) {
+                match = false;
+                break;
+            }
+        }
+
+        if (match) {
+            return text + text_i;
+        }
+    }
+    return NULL;
+}
+
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    size_t skip_tbl[ALPHABET_LEN];
+    size_t key_len = strlen(key);
+    size_t text_len = strlen(text);
+    for (size_t i = 0; i < ALPHABET_LEN; ++i) {
+        skip_tbl[i] = key_len;
+    }
+    for (size_t i = 0; i < key_len; ++i) {
+        skip_tbl[(size_t)key[i]] = key_len - 1 - i; 
+    }
 
+    size_t pos = key_len - 1;
+    while (pos < text_len) {
+        for (size_t i = 0; i < key_len; ++i) {
+            size_t key_pos = key_len - 1 - i;
+            if (key[key_pos] != text[pos - i]) {
+                size_t new_pos = pos - i + skip_tbl[(size_t)text[pos]];
+                if (new_pos <= pos) {
+                    new_pos = pos + 1;
+                }
+                pos = new_pos;
+                break;
+            }
+
+            if (key_pos == 0) {
+                return text + pos - i;
+            }
+        }
+    }
+
+    return NULL;
 }
 
 int main(void)
